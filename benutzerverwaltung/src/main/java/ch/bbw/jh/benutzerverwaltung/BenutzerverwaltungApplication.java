9f0151908318ba12d759ad58e611f684003b7a60
Jorin Heer <jorin.heer@gmail.com>
2022-11-11 10:40:37 +0100

    Initial commit

---
+++ benutzerverwaltung/src/main/java/ch/bbw/jh/benutzerverwaltung/BenutzerverwaltungApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.jh.benutzerverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenutzerverwaltungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenutzerverwaltungApplication.class, args);
+	}
+
+}
