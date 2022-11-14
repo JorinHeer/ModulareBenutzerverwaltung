ece98a6f3240d7b632fee099fbe45bcfa197b3d6
Jorin Heer <jorin.heer@gmail.com>
2022-11-14 15:42:17 +0100

    fix users.html to show users

--- benutzerverwaltung/src/main/java/ch/bbw/jh/benutzerverwaltung/BenutzerController.java
+++ benutzerverwaltung/src/main/java/ch/bbw/jh/benutzerverwaltung/BenutzerController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -16,6 +15,7 @@ public class BenutzerController {
     @GetMapping("/get-users")
     public String getRequestUsers(Model model) {
         System.out.println("getRequestUsers");
+        benutzerService.add(new Benutzer("name"," dfasdf", "123123", "sdfadf","qqwerqr","USER"));
         model.addAttribute("users", benutzerService.getAll());
         return "users";
     }
@@ -25,24 +25,14 @@ public class BenutzerController {
         Benutzer member = benutzerService.getById(id);
         System.out.println("editUser get: " + member);
      model.addAttribute("user", member);
-          return "changeuser";
+          return "edit";
     }
 
-    @PostMapping("/edit-member")
-    public String editMember(Benutzer benutzer, Model model) {
-
-        System.out.println("editUser post: edit user" + benutzer);
-        Benutzer value = benutzerService.getById(benutzer.getId());
-        value.setRole(benutzer.getRole());
-        System.out.println("editUser post: update user" + value);
-        benutzerService.update(benutzer.getId(), value);
-               return "redirect:/users";
-    }
 
     @GetMapping("/delete-user")
     public String deleteUser(@RequestParam(name = "id", required = true) long id, Model model) {
         System.out.println("deleteMember: " + id);
         benutzerService.deleteById(id);
-        return "redirect:/users";
+        return "redirect:/get-users";
     }
 }
