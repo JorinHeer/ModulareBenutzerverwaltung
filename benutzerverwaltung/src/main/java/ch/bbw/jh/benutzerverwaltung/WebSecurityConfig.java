33880c0f13af813168c36638045361224255ae70
Jorin Heer <jorin.heer@gmail.com>
2022-11-14 12:37:00 +0100

    fix typos

--- benutzerverwaltung/src/main/java/ch/bbw/jh/benutzerverwaltung/WebSecurityConfig.java
+++ benutzerverwaltung/src/main/java/ch/bbw/jh/benutzerverwaltung/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .antMatchers("/", "/home").permitAll()
+                        .antMatchers("/", "/index").authenticated()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
