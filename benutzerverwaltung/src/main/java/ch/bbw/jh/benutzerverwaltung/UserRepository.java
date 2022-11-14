68465809471f07403509c444e5be8706f1c0a57c
Jorin Heer <jorin.heer@gmail.com>
2022-11-14 10:46:39 +0100

    Revert "create userDAO"
    This reverts commit d0e81a8575ba868b1ebb5c2d4bdc81ff47f73234.

--- benutzerverwaltung/src/main/java/ch/bbw/jh/benutzerverwaltung/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.bbw.jh.benutzerverwaltung;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository                                                    //Klasse, id-Typ
-public interface UserRepository extends CrudRepository<User, Long> {
-    //Da wir eine embedded database verwenden, braucht es keine Conecction Information.
-}
